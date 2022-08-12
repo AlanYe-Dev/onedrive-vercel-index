0c99d8819300147ae6c42c698d73343fee7f54f4
_Wr_ <45558679+AlanYe-Dev@users.noreply.github.com>
2022-08-12 09:55:39 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,8 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
       <Head>
         <title>{siteConfig.title}</title>
-        <script src="/analytics.js" defer />
+        <script src="/analytics.js" async />
+        <script src="https://www.googletagmanager.com/gtag/js?id=G-JC17SK5FDW" async />
       </Head>
 
       <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
